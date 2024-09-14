748d84b5c44ad92aa921e3e9975d2049dbec38d7
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-09-15 00:40:17 +0330

    created portfolio data base and writed some codes in this part

---
+++ src/data/filterListData.js
@@ -0,0 +1,24 @@
+const filterListData = [
+    {
+        _id:1,
+        name:"All",
+        active:true,
+    },
+    {
+        _id:2,
+        name:"UX/UI",
+        active:false,
+    },
+    {
+        _id:3,
+        name:"Web",
+        active:false,
+    },
+    {
+        _id:4,
+        name:"App",
+        active:false,
+    },
+]
+
+export default filterListData
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './portfolio.css'
+import SectionTitle from '../components/SectionTitle'
+import filterListData from '../data/filterListData'
+
+//  http://localhost:3000/api/portfolioData.json 
 
 function Portfolio({reference}) {
+
+const [portfolio,setPortfolio]=useState([])
+const [data,setData]=useState([])
+const [filters,setFilters]=useState(filterListData)
+
+const fetchData =()=> {
+  fetch('http://localhost:3000/api/portfolioData.json')
+  .then(res => res.json())
+  .then(data => {
+    setPortfolio(data)
+    console.log(data);
+    
+  }
+  ).catch( e => console.log(e.message)
+  )
+}
+
+useEffect(()=> {
+  fetchData()
+},[])
+
+useEffect(()=> {
+  setData(portfolio)
+},[portfolio])
+
   return (
     <section id="portfolio" className="portfolio" ref={reference}>
-        <h1>Portfolio</h1>
+        <div className="container">
+          <SectionTitle title="Portfolio" subtitle="My Works"/>
+          <div className="row">
+            <div className="col-lg-12 d-flex justify-content-center">
+              <ul id="portfolio-filters">
+
+              </ul>
+            </div>
+          </div>
+          <div className="row portfolio-container">
+          {
+            data && data.length > 0 &&
+            data.map((item)=>(
+              <p key={item._id}>{item.category}</p>
+            ))
+          }
+          </div>
+
+        </div>
     </section>
   )
 }
