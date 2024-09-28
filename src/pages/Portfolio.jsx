810ed329fe7273d3b5f7e042c507a45de3886abc
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-09-29 01:05:18 +0330

    testing 2

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,7 +14,7 @@ const [data,setData]=useState([])
 const [filters,setFilters]=useState(filterListData)
 
 const fetchData =()=> {
-  // const axios =require('axios')
+  const axios =require('axios')
   axios.get('http://localhost:3000/api/portfolioData.json')
   .then(res => res.json())
   .then(data => {
