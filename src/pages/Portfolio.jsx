0c689beb0c119865daecc505132522e178f880c5
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-09-29 02:29:06 +0330

    test 14

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,7 +14,7 @@ const [data,setData]=useState([])
 const [filters,setFilters]=useState(filterListData)
 
 const fetchData =()=> {
-  fetch('/api/portfolioData.json')
+  fetch('http://localhost:3000/api/portfolioData.json')
   .then(res => res.json())
   .then(data => {
     setPortfolio(data)
